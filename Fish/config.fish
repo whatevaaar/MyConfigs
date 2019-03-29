5c013a59eae111fdf772494eec3bf1ab09e17f2e
Your Name <emiliano_hc@hotmail.com>
2019-03-29 11:25:46 -0600

    Updt 2 Termite

--- Fish/config.fish
+++ Fish/config.fish
@@ -1,3 +1,4 @@
+#put me in ~/.config/fish/config.fish
 alias idea="/home/nexus/Downloads/idea-IU-183.5912.21/bin/idea.sh"
 alias clion="/home/nexus/Downloads/clion-2018.3.4/bin/clion.sh"
 alias pycharm="/home/nexus/Downloads/pycharm-2018.3.5/bin/pycharm.sh"
