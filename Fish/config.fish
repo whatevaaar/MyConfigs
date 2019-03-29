40607efd8cb139fc68af02aba13670db64100875
Your Name <emiliano_hc@hotmail.com>
2019-03-29 11:10:01 -0600

    Updt 1 Fish alias

---
+++ Fish/config.fish
@@ -0,0 +1,7 @@
+alias idea="/home/nexus/Downloads/idea-IU-183.5912.21/bin/idea.sh"
+alias clion="/home/nexus/Downloads/clion-2018.3.4/bin/clion.sh"
+alias pycharm="/home/nexus/Downloads/pycharm-2018.3.5/bin/pycharm.sh"
+alias webstorm="/home/nexus/Downloads/WebStorm-183.5912.23/bin/webstorm.sh"
+alias harakiri="shutdown now"
+alias tryagain="shutdown now -r"
+alias aliasfish="vim ~/.config/fish/config.fish"
